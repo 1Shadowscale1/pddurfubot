ac7ba0ff2512e1b63b36fef9f29bd0f7f6b46610
1Shadowscale1 <81811251+1Shadowscale1@users.noreply.github.com>
2021-11-29 16:49:28 +0500

    fix

--- src/main/java/pddurfubot/handlers/CommandSwitch.java
+++ src/main/java/pddurfubot/handlers/CommandSwitch.java
@@ -10,10 +10,9 @@ public class CommandSwitch {
         int userId = Math.toIntExact(message.getFrom().getId());
         BotState botState = userDataCache.getUsersCurrentBotState(userId);
 
-        if (botState == BotState.START_EXAM | botState == BotState.QUESTION_EXAM)
-            botState = SwitchExam.SwitchExamCommands(botState, message);
-        else
-            botState = SwitchBasic.SwitchBasicCommands(botState, message);
+        botState = SwitchExam.SwitchExamCommands(botState, message);
+        /* botState = SwitchBasic.SwitchBasicCommands(botState, message);
+        */
 
         userDataCache.setUsersCurrentBotState(userId, botState);
         return CommandBuild.BuildOutputMessage(botState, message, userId);
