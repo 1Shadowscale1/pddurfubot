a9449724b631ef95623f81092bddcd36726e906f
Sallo0 <simiklassnik@yandex.ru>
2021-11-29 21:20:14 +0500

    return main

--- src/main/java/pddurfubot/Main.java
+++ src/main/java/pddurfubot/Main.java
@@ -4,7 +4,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Initializing {
+public class Main {
     public static void main(String[] args) throws TelegramApiException {
         Bot bot = new Bot();
         TelegramBotsApi botapi = new TelegramBotsApi(DefaultBotSession.class);
