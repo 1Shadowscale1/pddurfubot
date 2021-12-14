5653f1ef6d5e34f6c37802d9321d3e3336c3dbab
Sallo0 <simiklassnik@yandex.ru>
2021-12-14 06:18:44 +0500

    connect dataBase

--- src/main/java/pddurfubot/Main.java
+++ src/main/java/pddurfubot/Main.java
@@ -3,9 +3,14 @@ package pddurfubot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import pddurfubot.db.DataBase;
 
 public class Main {
+
+
     public static void main(String[] args) throws TelegramApiException {
+        DataBase.initialise();
+        DataBase.openSession();
         Bot bot = new Bot();
         TelegramBotsApi botapi = new TelegramBotsApi(DefaultBotSession.class);
         try {
---
+++ src/main/java/pddurfubot/db/DataBase.java
@@ -0,0 +1,50 @@
+package pddurfubot.db;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import pddurfubot.exam.ExamQuestion;
+
+public class DataBase {
+    private static Configuration configuration;
+    private static StandardServiceRegistryBuilder builder;
+    private static SessionFactory sessionFactory;
+    private static Session session;
+
+    public static void initialise(){
+        configuration = new Configuration().configure();
+        configuration.addAnnotatedClass(ExamQuestion.class);
+        builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        sessionFactory =configuration.buildSessionFactory(builder.build());
+
+    }
+
+    public static void openSession(){
+        session = sessionFactory.openSession();
+    }
+
+    public static void closeSession(){
+        session.close();
+    }
+
+    public static ExamQuestion getExamQuestion(int id){
+        Transaction transaction1 = session.beginTransaction();
+        ExamQuestion examQuestion = session.find(ExamQuestion.class,id);
+        transaction1.commit();
+        return examQuestion;
+    }
+
+    public static Integer getVariantsCount(){
+        return 1;
+    }
+
+    public static ExamQuestion[] getVariant(Integer variant){
+        ExamQuestion[] questions = new ExamQuestion[20];
+        for (int i = 0; i < 20; i++){
+            questions[i] = DataBase.getExamQuestion(i+1);
+        }
+        return questions;
+    }
+}
