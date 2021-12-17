0f9b1589e0ead9062cf222c8ffac32da3293ad9d
1Shadowscale1 <81811251+1Shadowscale1@users.noreply.github.com>
2021-12-17 07:37:18 +0500

    Revert "Implementing weatherAPI"
    This reverts commit 33f59a9a877f93ec037900015a598598b9ade14b.

--- main/java/pddurfubot/cache/DataCacheInterface.java
+++
@@ -1,9 +0,0 @@
-package pddurfubot.cache;
-
-import pddurfubot.handlers.BotState;
-
-public interface DataCacheInterface {
-    void setUsersCurrentBotState(Long userId, BotState botState);
-
-    BotState getUsersCurrentBotState(Long userId);
-}
