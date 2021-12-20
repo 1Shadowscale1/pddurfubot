61b3be426428255269a942e7d77f66a37e646056
1Shadowscale1 <81811251+1Shadowscale1@users.noreply.github.com>
2021-12-21 00:06:03 +0500

    Questions support with colored text

--- src/main/java/pddurfubot/exam/FormHtmlStats.java
+++ src/main/java/pddurfubot/exam/FormHtmlStats.java
@@ -1,27 +1,57 @@
 package pddurfubot.exam;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 public class FormHtmlStats {
     public static File FormHtml(String date, int variant, int result,
                                 List<AnsweredExamQuestion> answeredQuestions) throws IOException {
-        File htmlTemplateFile = new File("src\\main\\resources\\exam-stats.html");
+        File htmlTemplateFile = new File("src/main/resources/exam-stats.html");
         String htmlString = FileUtils.readFileToString(htmlTemplateFile);
 
         String head = "Дата: " + date;
         String subHead = "Вариант: " + variant;
-        String examResult = "Результат " + result + "из 20.";
+        String examResult = "Результат " + result + " " + "из" + answeredQuestions.size() + ".";
         htmlString = htmlString.replace("$head", head);
         htmlString = htmlString.replace("$subhead", subHead);
         htmlString = htmlString.replace("$result", examResult);
-        File newHtmlFile = new File("src\\main\\resources\\exam-stats-new.html");
+
+        int i;
+        for (i = 0; i < answeredQuestions.size(); i++) {
+            AnsweredExamQuestion answer = answeredQuestions.get(i);
+            StringBuilder pic = new StringBuilder("<p><img src=\"data:image/jpg;base64, " +
+                    imageToTxt(answer.getImageFile().getPath()) +
+                    "\" alt=\"pddQuestionImage\" width=\"300\" height=\"200\"></p>");
+            int j;
+            for (j = 0; j < answer.answers.size(); j++){
+                List<String> answers = answer.getAnswers();
+                pic.append("<p");
+                if (answer.correctAnswer.equals(String.valueOf(answers.get(j).charAt(0))))
+                    pic.append(" style=\"color:#86dc3d\"");
+                else if (answer.getUserAnswer().equals(String.valueOf(answers.get(j).charAt(0))))
+                    pic.append(" style=\"color:#ff0000\"");
+                pic.append(">").append(answers.get(j)).append("</p>");
+            }
+            int index = htmlString.indexOf("</body>");
+            htmlString = new StringBuilder(htmlString).insert(index, pic).toString();
+        }
+
+        htmlString = new StringBuilder(htmlString).insert(htmlString.indexOf("</body>"),
+                "<p>" + examResult + "</p>").toString();
+
+        File newHtmlFile = new File("src/main/resources/exam-stats-new.html");
         FileUtils.writeStringToFile(newHtmlFile, htmlString);
         return newHtmlFile;
+    }
+
+    private static String imageToTxt(String path) throws IOException{
+        File file = new File(path);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return new String(Base64.encodeBase64(bytes), "UTF-8");
     }
 }
