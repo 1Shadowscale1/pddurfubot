9562095607b1ff204af33bfb9ae661e7e596ee0d
1Shadowscale1 <81811251+1Shadowscale1@users.noreply.github.com>
2021-12-20 19:49:17 +0500

    forming html stats

---
+++ src/main/java/pddurfubot/exam/FormHtmlStats.java
@@ -0,0 +1,30 @@
+package pddurfubot.exam;
+
+import org.apache.commons.io.FileUtils;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class FormHtmlStats {
+    public static SendDocument FormHtml(String date, int variant, int result,
+                                List<AnsweredExamQuestion> answeredQuestions) throws IOException {
+        File htmlTemplateFile = new File("src\\main\\resources\\exam-stats.html");
+        String htmlString = FileUtils.readFileToString(htmlTemplateFile);
+
+        String head = "Дата: " + date;
+        String subHead = "Вариант: " + variant;
+        String examResult = "Результат " + result + "из 20.";
+        htmlString = htmlString.replace("$head", head);
+        htmlString = htmlString.replace("$subhead", subHead);
+        htmlString = htmlString.replace("$result", examResult);
+        File newHtmlFile = new File("src\\main\\resources\\exam-stats-new.html");
+        FileUtils.writeStringToFile(newHtmlFile, htmlString);
+
+        SendDocument msg = new SendDocument();
+        msg.setDocument(new InputFile().setMedia(new File("src\\main\\resources\\exam-stats-new.html")));
+        return msg;
+    }
+}
